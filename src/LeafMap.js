1fbc06f7c94308f2848bd82a7704e67c540f7d05
Jennifer Weber <weberjc@MacBookPro.attlocal.net>
2025-06-28 18:37:45 -0700

    LumiColors

--- src/LeafMap.js
+++ src/LeafMap.js
@@ -237,12 +237,47 @@ function renderPopup(item, handleStreamIndexButtonClick, streamSubIndex, useVide
 }
 
 function renderMarkerText(item, zoomLevel) {
-  console.log(item.player1Info.nameWithRomaji)
   var lumitier = item.bracketInfo.lumitier
   // var lumitier = "C+"
   var lumitierStr = ''
   if (lumitier != null && lumitier.length > 0) {
-    lumitierStr = `<span style="background:rgb(8, 83, 181); font-size: 13px; padding-left: 5px; padding-right: 5px; padding-bottom: 2px; margin-left: -2px; margin-top: 80px; border-radius: 6px; border: 1px solid rgb(7, 41, 87); color: #fff">${lumitier}</span>`
+    var lumiColor;
+    console.log(lumitier.charAt(0))
+    //reference:
+      // charEmojiImagePathase 'D':
+      //   lumiColor = "rgb(119, 195, 223)"; break;
+      // case 'C':
+      //   lumiColor = "rgb(132, 227, 122)"; break;
+      // case 'B':
+      //   lumiColor = "rgb(221, 229, 114)"; break;
+      // case 'A':
+      //   lumiColor = "rgb(220, 166, 103)"; break;
+      // case 'S':
+      //   lumiColor = "rgb(210, 100, 96)"; break;
+      // case 'P':
+      //   lumiColor = "rgb(119, 195, 223)"; break;
+      // default:
+      //   lumiColor = "rgb(8, 83, 181)";
+    // lumiColor = "#bdc636"; break;
+
+    switch (lumitier.charAt(0)) {
+      case 'D':
+        lumiColor = "#77c3df"; break;
+      case 'C':
+        lumiColor = "#84e37a"; break;
+      case 'B':
+        lumiColor = "#bdc636"; break;
+      case 'A':
+        lumiColor = "#dca667"; break;
+      case 'S':
+        lumiColor = "#d26460"; break;
+      case 'P':
+        lumiColor = "#77c3df"; break;
+      default:
+        lumiColor = "#0853b5";
+    }
+    console.log("color: ", lumiColor)
+    lumitierStr = `<span style="background:${lumiColor}; font-size: 13px; padding-left: 5px; padding-right: 5px; padding-bottom: 2px; margin-left: -2px; margin-top: 80px; border-radius: 6px; border: 1px solid rgb(7, 41, 87); color: #fff">${lumitier}</span>`
   }
   
   var txt = `<div style="color: black; line-height:1.2;margin-left: -150px; margin-top: -2px; font-size: 10px; font-weight: bold; width:300px; flex; align-items: center; justify-content: center; position: relative;">
